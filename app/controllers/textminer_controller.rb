ed531b4fed27fa2329121dc3d4e7a8b727ec2b1d
tomohitoy <t07840ty@gmail.com>
2013-09-16 00:04:45 +0900

    back

--- app/controllers/textminer_controller.rb
+++ app/controllers/textminer_controller.rb
@@ -1,15 +1,5 @@
 class TextminerController < ApplicationController
   before_action :authenticate_user!
   def index
-  end
-  def mining
-    RubyPython.start(:python_exe => "python2.7.5")
-    cPickle = RubyPython.import("cPickle")
-    sys = RubyPython.import 'sys'
-    sys.path.append("#{Rails.root}/lib")
-    mining = RubyPython.import('evaluate_tweets')
-    @mined = mining.twitter_search('test', current_user.access_token, current_user.access_token_secret)
-    RubyPython.stop
-  end
-  
+  end  
 end
